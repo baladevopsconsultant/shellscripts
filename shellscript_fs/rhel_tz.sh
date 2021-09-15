a6a5152f1c74f978c9be7ba7096f94f808d8a261
Bala <Bala@gmail.com>
2021-09-15 16:22:38 +0530

    Linux TZ initial Script Draft

---
+++ shellscript_fs/rhel_tz.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+rhel_ver=$(awk -F'=' '/VERSION_ID/{ gsub(/"/,""); print $2}' /etc/os-release)
+TZ=Asia/Kolkata
+rhel_ver_7(){
+ Hostname=$(hostname -f) 
+ Date=$(date)
+ timedatectl set-timezone $TZ
+ echo "Timezone set for $Hostname"
+ echo "==============================="
+ echo $Hostname:$Date
+} 
+
+if (( $rhel_ver==7 && $rhel_ver<8 ))
+then
+rhel_ver_7
+fi
